a917c35b3148de72244802f9a50d5362590e235f
Josh Akeman <joshakeman@gmail.com>
2019-06-04 16:54:46 -0400

    set basic app up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import cswGif from './images/csw_gif.gif'
+
+function App() {
+  return (
+    <div className="container">
+      <img src={require('./images/csw_gif.gif')} />
+    </div>
+  );
+}
+
+export default App;
